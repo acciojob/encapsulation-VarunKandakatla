2e0e2eb33b761e3f62187b140b75187fadea679c
Varun-Kandakatla <varunkumarkandakatla@gmail.com>
2023-01-13 21:37:21 +0530

    1st Sol

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
-  
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+        obj.setMember(sc.nextInt());
+        System.out.println(obj.getMember());
+    }
 }

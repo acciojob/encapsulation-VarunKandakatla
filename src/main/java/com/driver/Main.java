60ec05d420be0f35a15b726a35e54876a7345c37
Varun-Kandakatla <varunkumarkandakatla@gmail.com>
2023-01-13 22:22:39 +0530

    OOPS

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,10 +6,8 @@ public class Main {
     static Scanner sc=new Scanner(System.in);
     public static void main(String[] args) {
         RWOnly obj=new RWOnly();
-//        obj.member =sc.next();
-//        System.out.println(obj.member);
 
-        obj.setName(sc.nextInt());
+        obj.setName(sc.next());
         System.out.println(obj.getName());
     }
 }
